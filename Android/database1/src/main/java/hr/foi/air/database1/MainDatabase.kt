4215cc768097683d6e4fbaa3b6850ad488690dda
Marin Oroz <moroz@foi.hr>
2023-02-08 17:07:22 +0100

    Izrada MainDatabase.kt

--- Android/database1/src/main/java/hr/foi/air/database1/MainDatabase.kt
+++ Android/database1/src/main/java/hr/foi/air/database1/MainDatabase.kt
@@ -1,4 +1,20 @@
 package hr.foi.air.database1
 
-class MainDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import hr.foi.air.database1.entities.*
+
+@Database(version = 1, entities = [Car::class, IsAppearing::class,Obstacle::class,ObstacleType::class,Ride::class], views = [], exportSchema = false)
+abstract class MainDatabase : RoomDatabase() {
+    abstract fun getDao() : DAO
+
+    companion object{
+        private var instance : MainDatabase? = null
+
+        fun getInstance() : MainDatabase{
+            if (instance == null){
+                instance 
+            }
+        }
+    }
 }

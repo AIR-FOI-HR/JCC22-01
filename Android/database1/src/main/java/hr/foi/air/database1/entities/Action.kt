6177f9d07a8aae692ccaf8f1e23b2fac2d2e97f3
Marin Oroz <moroz@foi.hr>
2023-02-08 21:58:28 +0100

    Ispravljanje baze podataka te unošenje podataka za demonstraciju prvog primjera. Kreiranje nove "User" tablice.

--- Android/database1/src/main/java/hr/foi/air/database1/entities/Action.kt
+++ Android/database1/src/main/java/hr/foi/air/database1/entities/Action.kt
@@ -3,10 +3,9 @@ package hr.foi.air.database1.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "obstacleTypes")
-data class ObstacleType(
+@Entity(tableName = "actions")
+data class Action(
     @PrimaryKey(autoGenerate = true)
     var id : Int? = null,
-    var name : String = "",
-    var action : String = ""
+    var name : String = ""
 )

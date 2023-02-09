c85b9c014cbc088c469b535c57cacd9211c22312
Antonio Lipovac <alipovac@foi.hr>
2023-02-10 00:55:18 +0100

    Login pomocu baze

--- Android/app/src/main/java/hr/foi/air/autosimulation/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/autosimulation/MainActivity.kt
@@ -2,6 +2,7 @@ package hr.foi.air.autosimulation
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
@@ -11,8 +12,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import hr.foi.air.autosimulation.databinding.ActivityMainBinding
 import hr.foi.air.autosimulation.databinding.FragmentMainBinding
+import hr.foi.air.database1.MainDatabase
 import hr.foi.air.database1.data.DataRepository
 import hr.foi.air.database1.data.MockData
+import hr.foi.air.database1.entities.User
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,15 +26,24 @@ class MainActivity : AppCompatActivity() {
         val username = findViewById<EditText>(R.id.username)
         val password = findViewById<EditText>(R.id.password)
 
-        loginbtn.setOnClickListener {
-            if (username.text.toString() == "admin" && password.text.toString() == "admin") {
-                Toast.makeText(this, "Uspjesna prijava", Toast.LENGTH_SHORT).show()
-            } else {
+        var db = MainDatabase.getInstance(this)
+        val usersList : List<User> = db.getDao().getAllUsers()
+
+        loginbtn.setOnClickListener{
+            if(username.text.isNotBlank() && password.text.isNotBlank()){
+                for(user in usersList){
+                    if(user.username == username.text.toString() && user.password == password.text.toString()){
+                        Toast.makeText(this, "Uspjesna prijava", Toast.LENGTH_SHORT).show()
+                        Log.d("Provjera", "Uspjesna prijava")
+                    }
+
+                }
+            }
+            else{
                 Toast.makeText(this, "Neuspjesna prijava", Toast.LENGTH_SHORT).show()
             }
         }
-        setupDatabase()
-        //}
+
     }
 
     private fun setupDatabase(/*binding: FragmentMainBinding*/) {
